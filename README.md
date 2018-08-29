81113b5e629975f1107f06e7c5514e322293c57c
Nicolay Poymanov <n.poymanov@gmail.com>
2018-08-29 21:55:16 +0300

    add app logic

--- README.md
+++ README.md
@@ -1,7 +1,47 @@
 # Github Trends
 
-[TODO. There will be project description]
+Скрипт выводит список созданных за последнюю неделю **Github**-репозиториев с данными о количестве **"звезд"** и открытых **issues** для них.
 
-# Project Goals
+# Предварительные настройки
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+- Установить и запустить [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) для Python
+- Установить дополнительные пакеты:
+```
+pip install -r requirements.txt
+```
+
+# Как запустить
+
+Скрипт требует для своей работы установленного интерпретатора **Python** версии **3.5**.
+
+**Запуск на Linux**
+
+```bash
+$ python github_trending.py # или python3, в зависимости от настроек системы
+
+# результат выполнения скрипта
+https://github.com/felixrieseberg/windows95
+Stars: 9928
+Open Issues: 49
+
+https://github.com/GoogleChromeLabs/size-plugin
+Stars: 861
+Open Issues: 8
+
+https://github.com/iReaderAndroid/X2C
+Stars: 804
+Open Issues: 7
+
+# в случае ошибки запроса данных
+Failed to connect to Github
+
+# в случае ошибки загрузки данных запроса
+Failed to load repositories data (incorrect format)
+
+```
+
+Запуск на **Windows** происходит аналогично.
+
+# Цели проекта
+
+Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,62 @@
+import requests
+import sys
+import datetime
+import json
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_request_date():
+    today = datetime.date.today()
+    return today - datetime.timedelta(days=7)
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_request_data():
+    request_date = get_request_date()
+    params = {'q': 'created:>={}'.format(request_date), 'sort': 'stars',
+              'order': 'desc'}
+    url = 'https://api.github.com/search/repositories'
+
+    return url, params
+
+
+def get_request():
+    request_success_status = 200
+    url, params = get_request_data()
+
+    request = requests.get(url, params=params)
+
+    if not request.status_code == request_success_status:
+        return None
+
+    return request
+
+
+def load_data(request):
+    try:
+        repositories_data = json.loads(request.text)
+        return repositories_data['items']
+    except json.decoder.JSONDecodeError:
+        return None
+
+
+def output_repositories_to_console(json_data):
+    count_to_output = 20
+
+    print('Trending repositories:', '\n')
+    for repository in repositories_data[:count_to_output]:
+        template = ('{html_url}\nStars: {stargazers_count}\nOpen Issues: '
+                    '{open_issues_count}\n').format(**repository)
+        print(template)
+
 
 if __name__ == '__main__':
-    pass
+    request = get_request()
+
+    if not request:
+        sys.exit('Failed to connect to Github')
+
+    repositories_data = load_data(request)
+
+    if not repositories_data:
+        sys.exit('Failed to load repositories data (incorrect format)')
+
+    output_repositories_to_console(repositories_data)
